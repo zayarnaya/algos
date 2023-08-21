dbd5d0b4505233d1e13305acbe0cde45c176ee41
Veronika <veronika.zayarnaya@yandex.ru>
2023-08-21 20:41:40 +0300

    bfs для графа - с пометками

---
+++ bfs-graph.js
@@ -0,0 +1,70 @@
+// обход для графов - с пометками о посещении
+function Queue() {
+    this.head;
+    this.size = 0;
+
+    this.push = (value) => {
+        if (!this.head) {
+            this.head = {
+                value: value,
+                next: null,
+            }
+        } else {
+            let node = {
+                value: value,
+                next: this.head,
+            }
+            this.head = node;
+        }
+        this.size++;
+    }
+    this.shift = () => {
+        this.size--;
+        if (!this.head) {
+            return null;
+        } else {
+            let node = this.head;
+            let next = this.head.next;
+            this.head = next;
+            return node.value;
+        }
+
+    }
+}
+
+const bfs = (root, callback) => {
+    let queue = new Queue();
+    queue.push(root);
+    let visited = [];
+
+    while (queue.size) {
+        let node = queue.shift();
+        if (!visited.includes(node.value)) {
+            callback(node.value);
+            if (node?.children) {
+                for (let child of node.children) {
+                    queue.push(child);
+                }
+            };
+            visited.push(node.value);
+        }
+    }
+
+}
+
+bfs({
+    value: 0,
+    children: [{
+        value: 1,
+        children: [{
+            value: 2,
+            children: []
+        }]
+    }, {
+        value: 3,
+        children: [{
+            value: 0,
+            children: []
+        }]
+    }]
+}, (val) => console.log('val', val))
