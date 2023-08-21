b18e7681ddc5aebbf8afba39175892b18b98d4f3
Veronika <veronika.zayarnaya@yandex.ru>
2023-08-21 18:50:57 +0300

    сортировка выбором

---
+++ choiceSort.js
@@ -0,0 +1,14 @@
+const func = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = Infinity;
+        let index = -1;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < min) {
+                min = arr[j];
+                index = j;
+            }
+        }
+        [arr[i], arr[index]] = [arr[index], arr[i]];
+    }
+    console.log(arr);
+}
