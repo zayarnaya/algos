7be86287b913d496108ab50a0ff9d9efaf60b475
Veronika <veronika.zayarnaya@yandex.ru>
2023-08-21 20:37:08 +0300

    bfs2

--- bfs.js
+++ bfs.js
@@ -1,12 +1,50 @@
+function Queue() {
+    this.head;
+    this.size = 0;
+
+    this.push = (value) => {
+        if (!this.head) {
+            this.head = {
+                value: value,
+                next: null,
+            }
+        } else {
+            let node = {
+                value: value,
+                next: this.head,
+            }
+            this.head = node;
+        }
+        this.size++;
+    }
+    this.shift = () => {
+        this.size--;
+        if (!this.head) {
+            return null;
+        } else {
+            let node = this.head;
+            let next = this.head.next;
+            this.head = next;
+            return node.value;
+        }
+
+    }
+}
+
 const bfs = (root, callback) => {
-    const queue = [];
+    let queue = new Queue();
     queue.push(root);
-    while (queue.length) {
+
+    while (queue.size) {
         let node = queue.shift();
         if (node) {
             callback(node.value);
         }
-        if (node?.children) queue.push(...node.children);
+        if (node?.children) {
+            for (let child of node.children) {
+                queue.push(child);
+            }
+        };
     }
 
 }
