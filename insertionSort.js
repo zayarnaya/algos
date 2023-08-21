c1c1b8446a26e3eeb7bba2e3f6af6b7eb0ce6e47
Veronika <veronika.zayarnaya@yandex.ru>
2023-08-21 19:16:52 +0300

    insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,22 @@
+const func = (arr) => {
+    const sorted = [arr[0]];
+    let index = 1;
+    while (index < arr.length) {
+        let num = arr[index];
+        sorted.push(num);
+        for (let i = 0; i < sorted.length - 1; i++) {
+            if (sorted[i] > num) {
+                for (let k = sorted.length - 1; k >= i; k--) {
+                    sorted[k] = sorted[k - 1];
+                }
+                sorted[i] = num;
+                break;
+            }
+        }
+
+        index++;
+    }
+    return sorted;
+}
+
+console.log(func([4, 2, 7, 3, 9, 3]))
