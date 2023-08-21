e15543658fac2cc6f9e624c28945bbc3fc7c921d
Veronika <veronika.zayarnaya@yandex.ru>
2023-08-21 18:24:11 +0300

    bubbleSort

---
+++ bubblesort.js
@@ -0,0 +1,13 @@
+const func = (arr) => {
+    let count = 0;
+    do {
+        count = 0;
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+                count++;
+            }
+        }
+    } while (count > 0);
+    console.log(arr);
+}
